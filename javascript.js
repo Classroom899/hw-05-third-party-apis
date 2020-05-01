17e05be1d3bc509d4d62bda8f9261a87d58ae4fe
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-30 19:52:18 -0700

    Add for loop for timeToCompare and add save to local storage ability

--- javascript.js
+++ javascript.js
@@ -2,21 +2,35 @@ console.log(moment().format('L'));
 var currentDate = moment().format('L');
 $("#currentDay").text(currentDate);
 
-var currentEvent = moment().format('LTS');
-$("currentEvent")
+
 
 
 //Past events
-moment().subtract(6, 'days').calendar(); // Last Friday at 5:25 PM
+moment().subtract(60, "m"); // Last Friday at 5:25 PM
+var pastEvent = moment().subtract(60, "m");
+console.log(pastEvent);
 
 //Current time events
-moment().calendar(); //Today at 5:25 PM
+moment(); //Today at 5:25 PM
+var currentEvent = moment().startOf("hour");
+console.log(currentEvent);
 
 //Future time events 
-moment().add(3, 'days').calendar(); //Sunday at 5:25 PM
+moment().add(60, "m"); //Sunday at 5:25 PM using m for moment
+var futureEvent = moment().add(60, "m");
+console.log(futureEvent);
 
 
 //Need to save event in local storage
+$("#firstButton").click(function () {
+    $("#firstFormGroup").val();
+    var firstLocalStorageButton = $("#firstFormGroup").val();
+    console.log(firstLocalStorageButton);
+    localStorage.setItem("first", firstLocalStorageButton);
+})
+
+
+//Set every single event of every hour with each id and change that id
 
 
 
@@ -27,4 +41,23 @@ moment().add(3, 'days').calendar(); //Sunday at 5:25 PM
 
 
 
-//Add classes for past, present, and future
+//Add classes for past, present, and future so just use one for loop
+for (var i = 1; i < 4; i++) {
+    var colorEvent = $("#" + i).data().time
+    var indexEvent = $("#" + i)
+    console.log(colorEvent);
+    var timeToCompare = moment(colorEvent, 'h:mma');
+    if (timeToCompare < currentEvent) {
+        indexEvent.css({ "background-color": "#d3d3d3" })
+    }
+    else if (timeToCompare > currentEvent) {
+        indexEvent.css({ "background-color": "#77dd77" })
+    }
+    else if (timeToCompare === currentEvent) {
+        indexEvent.css({ "background-color": "#ff6961" })
+    }
+
+
+}
+
+//.data will access those objects for us
