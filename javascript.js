0ae27df1c3017213228769a49b84fc153fdcfbd3
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-30 17:27:19 -0700

    Add moment times

--- javascript.js
+++ javascript.js
@@ -2,5 +2,17 @@ console.log(moment().format('L'));
 var currentDate = moment().format('L');
 $("#currentDay").text(currentDate);
 
-var currentTime = moment().format('LTS');
-$("current")
+var currentEvent = moment().format('LTS');
+$("currentEvent")
+
+
+//Past events
+moment().subtract(6, 'days').calendar(); // Last Friday at 5:25 PM
+
+//Current time events
+moment().calendar(); //Today at 5:25 PM
+
+//Future time events 
+moment().add(3, 'days').calendar(); //Sunday at 5:25 PM
+
+
